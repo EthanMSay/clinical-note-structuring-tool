30843605af33092e6c040c680946f59e4fdc4477
Ethan Say <emsay@usc.edu>
2026-05-01 16:10:15 -0700

    Update CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,10 @@ init_db()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://clinical-note-structuring-tool-seven.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
